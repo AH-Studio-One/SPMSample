e015e88c05429216cf599d75d1c968d4a57d998e
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-06-28 18:11:32 +0200

    Initial Commit

---
+++ Shared/SPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMSampleApp.swift
+//  Shared
+//
+//  Created by A H on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct SPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
