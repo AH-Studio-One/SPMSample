21823377b1835a7ee1fd66120c359383c4c9b076
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-06-29 09:06:55 +0200

    Add swift package

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import CustomTheme
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let viewModel = SlideViewModel(title: "hello", subTitle: "SMP", theme: DemoTheme())
+        SlideContentView(viewModel: viewModel)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SlideContentView(viewModel: SlideViewModel(title: "hello", subTitle: "SMP", theme: DemoTheme()))
+    }
+}
+
+struct DemoTheme: Theme {
+    var primaryColor: Color
+    
+    var secondaryColor: Color
+    
+    var bgColor: Color
+    
+    var primaryFont: Font
+    
+    var secondaryFont: Font
+    
+    init(primaryColor: Color = .blue,
+         secondaryColor: Color = .yellow,
+         bgColor: Color = .clear,
+         primaryFont: Font = .title,
+         secondaryFont: Font = .body) {
+        
+        self.primaryColor = primaryColor
+        self.secondaryColor = secondaryColor
+        self.bgColor = bgColor
+        self.primaryFont = primaryFont
+        self.secondaryFont = secondaryFont
     }
 }
