0866d9e4d1092a523132656062b9c697d5c24028
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-07-01 15:27:57 +0200

    Add sample spec for Onboarding view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 import CustomTheme
 
 struct ContentView: View {
-
     var body: some View {
-        let viewModel = SlideViewModel(title: "hello", subTitle: "SMP", theme: DemoTheme())
-        SlideContentView(viewModel: viewModel)
+        let theme1 = DemoTheme()
+        let theme2 = DemoTheme(primaryColor: .blue,
+                               secondaryColor: .pink,
+                               primaryFont: .largeTitle,
+                               secondaryFont: .caption2)
+
+        let viewModel1 = SlideViewModel(title: "hello", subTitle: "SMP", theme: theme1)
+        let viewModel2 = SlideViewModel(title: "hello", subTitle: "SMP")
+        let viewModel3 = SlideViewModel(title: "hello", subTitle: "SMP", theme: theme2)
+        
+        OnboardingView(slideViewModels: [viewModel1, viewModel2, viewModel3])
     }
 }
 
@@ -26,22 +34,18 @@ struct DemoTheme: Theme {
     var primaryColor: Color
     
     var secondaryColor: Color
-    
-    var bgColor: Color
-    
+        
     var primaryFont: Font
     
     var secondaryFont: Font
     
-    init(primaryColor: Color = .blue,
-         secondaryColor: Color = .yellow,
-         bgColor: Color = .clear,
+    init(primaryColor: Color = .green,
+         secondaryColor: Color = .red,
          primaryFont: Font = .title,
          secondaryFont: Font = .body) {
         
         self.primaryColor = primaryColor
         self.secondaryColor = secondaryColor
-        self.bgColor = bgColor
         self.primaryFont = primaryFont
         self.secondaryFont = secondaryFont
     }
